3707148cbf9f83210602a9eebe79bee6c9ff7e10
alexisvassquez <vasquez.a712@gmail.com>
2026-05-14 14:31:40 -0400

    Changed comment from template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all subprojects/modules.
+// Top-level build file and configuration options
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
